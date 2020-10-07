583c3de42b9d9aeb0822649c0ff8c3c9952b276b
ThomasR <thomasrutgers@gmail.com>
2020-10-07 13:03:08 +0200

    Update midicontroller.ino

--- midicontroller.ino
+++ midicontroller.ino
@@ -15,13 +15,13 @@ void setup() {
   servo.attach(9);
 }
 
-
 void controlChange(byte channel, byte control, byte value) {
   midiEventPacket_t event = {0x0B, 0xB0 | channel, control, value};
   MidiUSB.sendMIDI(event);
 }
 
 void loop() {
+  
   //read midi
   midiEventPacket_t rx;
   rx = MidiUSB.read();
